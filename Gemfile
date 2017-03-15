a1323db732b619a9cf7aa46159b05254cd8b4c5d
izumin5210 <masayuki@izumin.info>
2017-03-16 07:42:46 +0900

    chore(gems): Introduce devise gem

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'devise'
 gem 'doorkeeper'
 
 group :development, :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.generators do |g|
     g.javascripts false
     g.stylesheets false
