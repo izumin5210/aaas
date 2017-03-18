ba222f6e38a9cd3fa810be3e6f6d31e18e9089e7
izumin5210 <masayuki@izumin.info>
2017-03-18 13:14:57 +0900

    feat(controllers): Support login_name column in accounts/registration#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'dashboard#show'
 
-  devise_for :accounts
+  devise_for :accounts, controllers: {
+    registrations: 'accounts/registrations',
+  }
   use_doorkeeper
 end
