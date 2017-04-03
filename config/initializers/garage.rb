21a7f323feb7f609d6868ff220417e3ab44211f2
izumin5210 <masayuki@izumin.info>
2017-04-03 14:56:58 +0900

    refactor(config): Refactor initializers

--- config/initializers/garage.rb
+++ config/initializers/garage.rb
@@ -1,15 +1,22 @@
-Garage.configuration.strategy = Garage::Strategy::Doorkeeper
-
 Garage.configure {}
 
 Garage::TokenScope.configure do
+  register :public, desc: 'accessing publicly available data' do
+  end
+
   namespace :user do
-    register :read, desc: 'accessing publicly available data' do
+    register :read, desc: 'accessing user\'s data' do
       access :read, OauthApplication
     end
 
-    register :write, desc: 'reading and writing data' do
-      access :write, OauthApplication
+    register :write, desc: 'creating and updating user\'s data' do
     end
   end
+end
+
+Garage.configuration.strategy = Garage::Strategy::Doorkeeper
+
+Doorkeeper.configure do
+  default_scopes  :public
+  optional_scopes *Garage::TokenScope.optional_scopes
 end
