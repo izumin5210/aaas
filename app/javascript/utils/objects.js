5f6961b1334405b871ee0c234f8045593a30ef1a
izumin5210 <masayuki@izumin.info>
2017-04-09 20:36:09 +0900

    feat(javascript): Add createClient function

---
+++ app/javascript/utils/objects.js
@@ -0,0 +1,10 @@
+/* @flow */
+import { camelizeKeys, decamelizeKeys } from 'humps'
+
+export function camel2snake(obj: Object) {
+  return decamelizeKeys(obj, { split: /(?=[A-Z0-9])/ })
+}
+
+export function snake2camel(obj: Object) {
+  return camelizeKeys(obj)
+}
